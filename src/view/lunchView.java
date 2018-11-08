3cd0aedeaa998463e28749e7ce906515d43b801d
moneysoucie0 <42638513+moneysoucie0@users.noreply.github.com>
2018-11-08 19:44:33 +0100

    reinstalation des class

---
+++ src/view/lunchView.java
@@ -0,0 +1,12 @@
+package view;
+
+public class lunchView {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//menu m = new menu();
+		uimain f = new uimain();
+		
+	}
+
+}
---
+++ src/view/uimain.java
@@ -0,0 +1,65 @@
+package view;
+
+import java.awt.Color;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import model.User;
+
+public class uimain extends JFrame {
+	
+
+		
+		public uimain() {
+			
+			//private JLabel bienvenu, important, txtImportant, equipe, txtEquipe, activite, txtActivite;
+			
+			this.setBounds(500,50,500,500/16*9);
+			this.setTitle("acceuil");
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setBackground(Color.pink);
+			
+			JLabel bienvenue = new JLabel("Bienvenu Copain Suisse");
+			JLabel important = new JLabel("Important");
+			JLabel txtImportant = new JLabel("vous puez trop vous ete vire, perche' stai cercando il sole nell'abisso");
+			JLabel equipe = new JLabel("Equipe");
+			JLabel txtEquipe = new JLabel("Jaque Boulons, Richard Pastel, Liserac");
+			JLabel activite = new JLabel("Activit");
+			JLabel txtActivite = new JLabel ("manger des petit suisse, voir Martine, Vendre mes enfant, pendre un NAZI, vir un lama.");
+			
+			JPanel panel[]= {new JPanel(),new JPanel(),new JPanel(),new JPanel(),new JPanel(),new JPanel(),new JPanel(),new JPanel()};
+			
+			panel[1].add(bienvenue);
+			panel[2].add(important);
+			panel[3].add(txtImportant);
+			panel[4].add(equipe);
+			panel[5].add(txtEquipe);
+			panel[6].add(activite);
+			panel[7].add(txtActivite);
+			
+			for (int i = 1; i < 8; i++) {
+				panel[i].setOpaque(false);panel[0].add(panel[i]);
+			}
+			
+			BoxLayout b= new BoxLayout(panel[0], BoxLayout.Y_AXIS);
+			
+			this.setContentPane(panel[0]);
+			panel[0].setBackground(Color.green);
+			
+			this.setVisible(true);
+			
+			/*User u = new User(59);
+			
+			//u.Read(1);
+			
+			System.out.println("userfirstname : " + u.prenom);
+			System.out.println("username : " + u.nom);
+			System.out.println("userpassword : " + u.password);
+			System.out.println("userlogin : " + u.login);
+			System.out.println("userid : " + u.id_utilisateur);*/
+			
+		
+		}
+}
+	
