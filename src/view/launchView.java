7212b079ac3fbaed33163e5b86f5d84a4df92322
Melissa LAPLEIGNE <melissa.lapleigne@alten.com>
2018-11-20 15:19:08 +0100

    Menu devient une classe, mis dans la lauch view

--- src/view/launchView.java
+++ src/view/launchView.java
@@ -6,10 +6,11 @@ public class launchView {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//menu m = new menu();
+		menu m = new menu();
 		new uimain();
 		new Acceuil();
 		new ViewCompteRendu();
+		new Menu();
 		
 	}
 
