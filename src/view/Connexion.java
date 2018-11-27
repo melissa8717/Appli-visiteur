e58c5c89a790a77cb50e82e8aaac51a5120f3e34
Liserac <43575942+Liserac@users.noreply.github.com>
2018-11-27 14:04:04 +0100

    re-creation de la classe JTextFieldModif et Connexion refaite

--- src/view/Connexion.java
+++ src/view/Connexion.java
@@ -2,35 +2,43 @@ package view;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
+/*
+import appliVisiteur_interfaceGraphique.Conteneur;
+import appliVisiteur_interfaceGraphique.Fenetre;
+import appliVisiteur_interfaceGraphique.JTextFieldModif;
+import appliVisiteur_interfaceGraphique.Paragraphe;
+import appliVisiteur_interfaceGraphique.TitrePrincipale;*/
 import model.User;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 
 public class Connexion {
 
 	public Connexion() {
-		Fenetre fenetre = new Fenetre();
+Fenetre fenetre = new Fenetre();
+
+		
+		int largeurConteneur = 600;
 	
+		
 		Conteneur conteneur = new Conteneur();
+		conteneur.setPreferredSize(new Dimension(largeurConteneur,375));
 		
 		TitrePrincipale titrePrincipale = new TitrePrincipale("Connexion");
 		
 		Paragraphe paragrapheId = new Paragraphe("Identifiant : ");
 		
-		JTextField textFieldId = new JTextField(10);
-		textFieldId.setText("jdebelle");
+		JTextFieldModif textFieldId = new JTextFieldModif(10, 12);
 		
 		Paragraphe paragrapheMdp = new Paragraphe("Mot de passe : ");
 		
-		JTextField textFieldMdp = new JTextField(10);
-		textFieldMdp.setText("nvwqq");
+		JTextFieldModif textFieldMdp = new JTextFieldModif(10, 12);
 		
 		JButton boutonValider = new JButton("Connexion");
-		boutonValider.addActionListener(new ActionListener() {
+/*		boutonValider.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					String login = textFieldId.getText();
@@ -48,15 +56,17 @@ public class Connexion {
 				
 			}
 		});
-		
+*/		
 		JPanel ligneUne =  new JPanel();
 		ligneUne.setOpaque(false);
+		ligneUne.setPreferredSize(new Dimension(largeurConteneur, 150));
 		
 		ligneUne.add(titrePrincipale);
 		
 		
 		JPanel ligneDeux = new JPanel();
 		ligneDeux.setOpaque(false);
+		ligneDeux.setPreferredSize(new Dimension(largeurConteneur, 75));
 		
 		ligneDeux.add(paragrapheId);
 		ligneDeux.add(textFieldId);
@@ -64,6 +74,7 @@ public class Connexion {
 		
 		JPanel ligneTrois = new JPanel();
 		ligneTrois.setOpaque(false);
+		ligneTrois.setPreferredSize(new Dimension(largeurConteneur, 75));
 		
 		ligneTrois.add(paragrapheMdp);
 		ligneTrois.add(textFieldMdp);
@@ -71,14 +82,15 @@ public class Connexion {
 		
 		JPanel ligneQuatre = new JPanel();
 		ligneQuatre.setOpaque(false);
+		ligneQuatre.setPreferredSize(new Dimension(largeurConteneur, 75));
 		
 		ligneQuatre.add(boutonValider);
+		
 		
 		conteneur.add(ligneUne);
 		conteneur.add(ligneDeux);
 		conteneur.add(ligneTrois);
 		conteneur.add(ligneQuatre);
-		
 		fenetre.add(conteneur);
 		
 		
