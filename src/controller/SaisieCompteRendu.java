f6365435960f7dd145388eff7843cae9115387b5
mlapleigne <melgraphdesign@gmail.com>
2018-12-11 10:05:50 +0100

    ajout du model CR

--- src/controller/SaisieCompteRendu.java
+++ src/controller/SaisieCompteRendu.java
@@ -11,7 +11,7 @@ public class SaisieCompteRendu {
 	
 
 		public SaisieCompteRendu(String id_rapport, String date, String bilan, String motif, String id_utilisateur) {
-			super(id_rapport, date, bilan, motif,   id_utilisateur);
+			//super(id_rapport, date, bilan, motif,   id_utilisateur);
 	
 		
 	}
