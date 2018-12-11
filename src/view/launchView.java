13ba059d1d050362b02d57a243bb77d105f38276
Guillaume <gcerdan83@gmail.com>
2018-12-11 09:45:52 +0100

    Unmerge bin

--- src/view/launchView.java
+++ src/view/launchView.java
@@ -6,15 +6,8 @@ public class launchView {
 		// TODO Auto-generated method stub
 		//new uimain();
 		//new Acceuil();
-<<<<<<< HEAD
-		//new ViewCompteRendu();
+		//new ViewCompteRendu(); 
 		new PowerpointUI();
-=======
-		//ViewCompteRendu();
-		//new Connexion();
-		new PowerpointUI();
-
->>>>>>> 8c259dc5651cdcb01a298a2622f927f470cb23ab
 		//new Menu();
 		
 	}
