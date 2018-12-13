36f66537aef7b1e673b588db642372e8beacbe00
Guillaume <gcerdan83@gmail.com>
2018-12-13 13:08:40 +0100

    Classe Carte compte rendu

---
+++ src/view/CarteCompteRendu.java
@@ -0,0 +1,29 @@
+package view;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.util.Date;
+import java.awt.Dimension;
+
+
+public class CarteCompteRendu extends JPanel {
+    public CarteCompteRendu(Date date, String medecin, String nomMedicament) {
+
+        
+        
+        Conteneur conteneur = new Conteneur(); 
+
+        JLabel JLabelDate = new JLabel(String.valueOf(date));
+        JLabelDate.setPreferredSize(new Dimension(1000, 150));
+        JLabel JLabelMedecin = new JLabel(medecin);
+        JLabel JLabelNomMedicament = new JLabel(nomMedicament);
+
+        
+        
+        conteneur.add(JLabelDate);
+        conteneur.add(JLabelMedecin);
+        conteneur.add(JLabelNomMedicament);
+
+        this.add(conteneur);
+    }
+}
