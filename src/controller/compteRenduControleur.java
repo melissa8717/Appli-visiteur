2dc8d5078a4b61e40d04f65e7661132140f88799
moneysoucie0 <42638513+moneysoucie0@users.noreply.github.com>
2018-12-13 10:22:02 +0100

    controleur d'ajout de compte rendu fait

--- src/controller/compteRenduControleur.java
+++ src/controller/compteRenduControleur.java
@@ -1,15 +1,28 @@
 package controller;
 
+import java.sql.ResultSet;
 import java.sql.Statement;
+
+import javax.swing.JLabel;
+
+import com.mysql.jdbc.Connection;
+import controller.CnxBDDLocalhost;
 
 public class compteRenduControleur {
 
 	
-	public Boolean ajoutCompteRendu (String medecin, String Motif, String commentaire, String Date, int echantillon) {
+	public static boolean ajoutCompteRendu (String medecin, String Motif, String commentaire,String date, int echantillon) {
+		try {
+			Connection conn = (Connection) CnxBDDLocalhost.connecteur();
+			String requete = "INSERT INTO rapport(idRapport, date, bilan, motif, idUtilisateur) VALUES (1,'"+date+"','"+commentaire+"','"+Motif+"',"+connectionControleur.id_utilisateur+")";
+			Statement statement =  conn.createStatement();
+			int rep = statement.executeUpdate(requete);
+			return (rep>0);
+		}
 		
-		String requete = "INSERT INTO rapport(idRapport, date, bilan, motif, idUtilisateur) VALUES (1,'"+Date+"','"+commentaire+"','"+Motif+"',"+connectionControleur.id_utilisateur+")";
-		Statement statement = ;
-		return false;  
+		catch (Exception e){
+			return false;
+		}
 	}
 
 
