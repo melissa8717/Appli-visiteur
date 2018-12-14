5b95054404678930a26af6e3ead837358d1ad6a3
Zaimone <andyplouf@gmail.com>
2018-12-14 18:08:25 +0100

    Id medecin en corélation avec ce qu'on choisie dans la JComboBox de la saisie de compte rendu

--- src/controller/compteRenduControleur.java
+++ src/controller/compteRenduControleur.java
@@ -4,6 +4,8 @@ package controller;
 
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.mysql.jdbc.PreparedStatement;
 
@@ -16,7 +18,7 @@ import com.mysql.jdbc.Connection;
 public class compteRenduControleur {
 
 	
-
+	//CR Saisie Compte Rendu
 	public static boolean ajoutCompteRendu (int medecin, String Motif, String commentaire,String date, int echantillon,String Medicament) {
 		try {
 			Connection conn = (Connection) CnxBDD.connecteurUserLab();
@@ -33,6 +35,38 @@ public class compteRenduControleur {
 			System.out.println("marche pas chef");
 			return false;
 		}
+	}
+	
+	public static List<List> selectMedecin() {
+		try {
+			List<List> List_Medecins = new ArrayList<List>();
+			
+			
+			Connection conn = (Connection) CnxBDD.connecteurUserLab();
+			String requete = "SELECT idPraticien,nom FROM praticien;";
+			Statement statement =  conn.createStatement();
+			ResultSet resultat = statement.executeQuery(requete);
+			while(resultat.next()) {
+				List<String> unMedecin = new ArrayList<String>();
+				int idMed= resultat.getInt( "idPraticien" );
+				String nomMed= resultat.getString( "nom" );
+				unMedecin.add(Integer.toString(idMed));
+				unMedecin.add(nomMed);
+				List_Medecins.add(unMedecin);
+			}
+			
+			return List_Medecins;
+			
+			
+		}
+		
+		catch (Exception e){
+			System.out.println(e);
+			System.out.println("marche pas chef");
+			return null;
+		}
+		
+		
 	}
 	
 	
