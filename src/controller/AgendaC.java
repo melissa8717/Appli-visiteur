2b6f933e620bc779683682e46b408278d66f4b58
Guillaume <gcerdan83@gmail.com>
2019-02-25 14:35:02 +0100

    a ne pas garder

--- src/controller/AgendaC.java
+++ src/controller/AgendaC.java
@@ -23,26 +23,25 @@ import com.mysql.jdbc.Connection;
 import model.Connecteur;
 
 public class AgendaC {
-	public static boolean ajoutEvenement (String rapport, String dateDebutEvent, String dateFinEvent, String heureDebut, String heureFinC) {
+	public static boolean ajoutEvenement(String rapport, String dateDebutEvent, String dateFinEvent, String heureDebut,
+			String heureFinC) {
 		Statement statement = null;
 		ResultSet resultat = null;
-		try { 
+		try {
 			Connection conn = (Connection) Connecteur.connecteurUL;
-			
-			
-			
+
 			/* Requête d'insertion en base du compte rendu */
-			String requete = 
-					"INSERT INTO" + 
-					"`agenda`( `evenement`, `dateDebut`, `dateFin`, `idUtilisateur`, `heureDebut`, `heureFin`)" + 
-					"VALUES ('"+rapport+"','"+dateDebutEvent+"','"+dateFinEvent+"','"+connectionControleur.id_utilisateur+"', '"+heureDebut+"' ,'"+heureFinC+"')";
-			statement =  conn.createStatement();	
+			String requete = "INSERT INTO"
+					+ "`agenda`( `evenement`, `dateDebut`, `dateFin`, `idUtilisateur`, `heureDebut`, `heureFin`)"
+					+ "VALUES ('" + rapport + "','" + dateDebutEvent + "','" + dateFinEvent + "','"
+					+ connectionControleur.id_utilisateur + "', '" + heureDebut + "' ,'" + heureFinC + "')";
+			statement = conn.createStatement();
 
 			/* Exécution de la reqête */
 			int rep = statement.executeUpdate(requete);
-			Popup Succes = new Popup("Ajout", 800,200);
-			
-			JPanel panelSucces = new JPanel(); 
+			Popup Succes = new Popup("Ajout", 800, 200);
+
+			JPanel panelSucces = new JPanel();
 			JLabel labelSucces = new JLabel("L'évenement a été ajouté correctement !");
 			Font font = new Font("Open Sans", Font.PLAIN, 30);
 			// Définition du style
@@ -53,15 +52,13 @@ public class AgendaC {
 			panelSucces.setBackground(new Color(85, 239, 196));
 			panelSucces.setForeground(new Color(96, 191, 96));
 			return (rep > 0);
-			
-			
-			
+
 		}
-		
-		catch (Exception e){
-			Popup NotSucces = new Popup("Ajout : erreur", 800,100);
-			
-			JPanel panelNotSucces = new JPanel(); 
+
+		catch (Exception e) {
+			Popup NotSucces = new Popup("Ajout : erreur", 800, 100);
+
+			JPanel panelNotSucces = new JPanel();
 			JLabel labelNotSucces = new JLabel("L'évenement n'a pas été ajouté correctement !");
 			Font font = new Font("Open Sans", Font.PLAIN, 30);
 			labelNotSucces.setFont(font);
@@ -71,232 +68,214 @@ public class AgendaC {
 			panelNotSucces.setBackground(new Color(235, 77, 75));
 			panelNotSucces.setForeground(new Color(191, 48, 48));
 			return false;
-		}
-		finally {
+		} finally {
 			if (resultat != null) {
 				try {
 					resultat.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 			if (statement != null) {
 				try {
 					statement.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
 	}
 
-
-	
-	public static  List<List> consultationEvenement(int IdUser) {
+	public static List<List> consultationEvenement(int IdUser) {
 		Statement statement = null;
 		ResultSet resultat = null;
 		try {
 			List<List> List_CE = new ArrayList<List>();
 			Connection conn = (Connection) Connecteur.connecteurUL;
 
-		    /* Création de l'objet gérant les requêtes */
+			/* Création de l'objet gérant les requêtes */
 			statement = conn.createStatement();
-			
+
 			/* Requête récupérat les comptes rendus du user connecté */
-		    String requete = "SELECT evenement, dateDebut, dateFin, idUtilisateur, heureDebut, heureFin, idAgenda from agenda where idUtilisateur="+IdUser+";";
+			String requete = "SELECT evenement, dateDebut, dateFin, idUtilisateur, heureDebut, heureFin, idAgenda from agenda where idUtilisateur="
+					+ IdUser + ";";
 			resultat = statement.executeQuery(requete);
 
-		    /* Exécution d'une requête de lecture */
-			
-		
-		    /* Récupération des données du résultat de la requête de lecture */
-		    while(resultat.next()) {
+			/* Exécution d'une requête de lecture */
+
+			/* Récupération des données du résultat de la requête de lecture */
+			while (resultat.next()) {
 				List<String> event = new ArrayList<String>();
 
-	            String evenement = resultat.getString("evenement");
-	            Date dateD = resultat.getDate("dateDebut");
-	            Date dateFin = resultat.getDate("dateFin");
-	            int idUtilisateur = resultat.getInt("idUtilisateur");
-	            String heureDebut = resultat.getString("heureDebut");
-	            String heureFin = resultat.getString("heureFin");
-	            int idAgenda = resultat.getInt("idAgenda");
+				String evenement = resultat.getString("evenement");
+				Date dateD = resultat.getDate("dateDebut");
+				Date dateFin = resultat.getDate("dateFin");
+				int idUtilisateur = resultat.getInt("idUtilisateur");
+				String heureDebut = resultat.getString("heureDebut");
+				String heureFin = resultat.getString("heureFin");
+				int idAgenda = resultat.getInt("idAgenda");
 
-	            //convertir une date en string
-	            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	            
-	            String strDateD = dateFormat.format(dateD);
-	            String strDateF = dateFormat.format(dateFin);
-	            
-	            
-	            event.add(evenement);
-	            event.add(strDateD);
-	            event.add(strDateF);
-	            event.add(Integer.toString(idUtilisateur));
-	            event.add(heureDebut);
-	            event.add(heureFin);
-	            event.add(Integer.toString(idAgenda));
+				// convertir une date en string
+				DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+				String strDateD = dateFormat.format(dateD);
+				String strDateF = dateFormat.format(dateFin);
+
+				event.add(evenement);
+				event.add(strDateD);
+				event.add(strDateF);
+				event.add(Integer.toString(idUtilisateur));
+				event.add(heureDebut);
+				event.add(heureFin);
+				event.add(Integer.toString(idAgenda));
 				List_CE.add(event);
-	            
-	           
-				
+
 			}
-		   
 
 			return List_CE;
 
-		   
 		}
-		
-		    
-		catch (Exception e){
-			
-			
+
+		catch (Exception e) {
+
 			return null;
-		}
-		finally {
+		} finally {
 			if (resultat != null) {
 				try {
 					resultat.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 			if (statement != null) {
 				try {
 					statement.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
-		
-	
+
 	}
-	
-	public static  List<List> consultationLastEvenement(int IdUser) {
+
+	public static List<List> consultationLastEvenement(int IdUser) {
 		Statement statement = null;
 		ResultSet resultat = null;
 		try {
 			List<List> List_CLE = new ArrayList<List>();
 			Connection conn = (Connection) Connecteur.connecteurUL;
 
-			System.out.println("connection"+conn);
-		    /* Création de l'objet gérant les requêtes */
+			System.out.println("connection" + conn);
+			/* Création de l'objet gérant les requêtes */
 			statement = conn.createStatement();
-			
+
 			/* Requête récupérat les comptes rendus du user connecté */
-		    String requete = "SELECT evenement, dateDebut, dateFin, idUtilisateur, heureDebut, heureFin, idAgenda from agenda where idUtilisateur="+IdUser+" order by idAgenda DESC limit 1;";
+			String requete = "SELECT evenement, dateDebut, dateFin, idUtilisateur, heureDebut, heureFin, idAgenda from agenda where idUtilisateur="
+					+ IdUser + " order by idAgenda DESC limit 1;";
 			resultat = statement.executeQuery(requete);
 
-		    /* Exécution d'une requête de lecture */
-			
-		
-		    /* Récupération des données du résultat de la requête de lecture */
-		    while(resultat.next()) {
+			/* Exécution d'une requête de lecture */
+
+			/* Récupération des données du résultat de la requête de lecture */
+			while (resultat.next()) {
 				List<String> event = new ArrayList<String>();
 
-	            String evenement = resultat.getString("evenement");
-	            Date dateD = resultat.getDate("dateDebut");
-	            Date dateFin = resultat.getDate("dateFin");
-	            int idUtilisateur = resultat.getInt("idUtilisateur");
-	            String heureDebut = resultat.getString("heureDebut");
-	            String heureFin = resultat.getString("heureFin");
-	            int idAgenda = resultat.getInt("idAgenda");
+				String evenement = resultat.getString("evenement");
+				Date dateD = resultat.getDate("dateDebut");
+				Date dateFin = resultat.getDate("dateFin");
+				int idUtilisateur = resultat.getInt("idUtilisateur");
+				String heureDebut = resultat.getString("heureDebut");
+				String heureFin = resultat.getString("heureFin");
+				int idAgenda = resultat.getInt("idAgenda");
 
-	            //convertir une date en string
-	            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	            
-	            String strDateD = dateFormat.format(dateD);
-	            String strDateF = dateFormat.format(dateFin);
-	            
-	            
-	            event.add(evenement);
-	            event.add(strDateD);
-	            event.add(strDateF);
-	            event.add(Integer.toString(idUtilisateur));
-	            event.add(heureDebut);
-	            event.add(heureFin);
-	            event.add(Integer.toString(idAgenda));
+				// convertir une date en string
+				DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+				String strDateD = dateFormat.format(dateD);
+				String strDateF = dateFormat.format(dateFin);
+
+				event.add(evenement);
+				event.add(strDateD);
+				event.add(strDateF);
+				event.add(Integer.toString(idUtilisateur));
+				event.add(heureDebut);
+				event.add(heureFin);
+				event.add(Integer.toString(idAgenda));
 				List_CLE.add(event);
-	            
-	           
-				
+
 			}
-		   
 
 			return List_CLE;
 
-
-		   
 		}
-		
-		    
-		catch (Exception e){
-			
-			
+
+		catch (Exception e) {
+
 			return null;
 		}
-		
-	
+
 	}
-	
-	public static  boolean countEvenement(int IdUser, String debut) {
+
+	public static boolean countEvenement(int IdUser, String debut) {
 		Statement statement = null;
 		ResultSet resultat = null;
 		try {
 			List<List> List_Count = new ArrayList<List>();
-			Connection conn =(Connection) Connecteur.connecteurUL;
+			Connection conn = (Connection) Connecteur.connecteurUL;
 
-		    /* Création de l'objet gérant les requêtes */
+			/* Création de l'objet gérant les requêtes */
 			statement = conn.createStatement();
-			
+
 			/* Requête récupérat les comptes rendus du user connecté */
-		    String requete = "SELECT COUNT(idAgenda) as event FROM agenda where dateDebut = "+debut+" and idUtilisateur ="+IdUser+";";
+			String requete = "SELECT COUNT(idAgenda) as event FROM agenda where dateDebut = " + debut
+					+ " and idUtilisateur =" + IdUser + ";";
 			resultat = statement.executeQuery(requete);
 
-		    /* Exécution d'une requête de lecture */
-			
-		
-		    /* Récupération des données du résultat de la requête de lecture */
-		    while(resultat.next()) {
-		    	// return resultat.getInt(1);
+			/* Exécution d'une requête de lecture */
 
-		   
-		    }
+			/* Récupération des données du résultat de la requête de lecture */
+			while (resultat.next()) {
+				// return resultat.getInt(1);
+
+			}
 			return true;
 
 		}
-		
-		    
-		catch (Exception e){
-			
+
+		catch (Exception e) {
+
 			return true;
 
-			
-		}
-		finally {
+		} finally {
 			if (resultat != null) {
 				try {
 					resultat.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 			if (statement != null) {
 				try {
 					statement.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
-		
-	
+
 	}
-	
-	public static boolean updateEvent(int idAgendaInt, String textEvent, String dateDebut, String dateFin, int idUtilisateur, String heureDebut, String heureFin) {
+
+	public static boolean updateEvent(int idAgendaInt, String textEvent, String dateDebut, String dateFin,
+			int idUtilisateur, String heureDebut, String heureFin) {
 		Statement statement = null;
 		int resultat = 0;
 		try {
-			Connection conn =(Connection) Connecteur.connecteurUL;
+			Connection conn = (Connection) Connecteur.connecteurUL;
 
-		    /* Création de l'objet gérant les requêtes */
+			/* Création de l'objet gérant les requêtes */
 			statement = conn.createStatement();
-			String requete = "UPDATE agenda SET idAgenda="+idAgendaInt+",evenement='"+textEvent+"',dateDebut='"+dateDebut+"',dateFin='"+dateFin+"',idUtilisateur="+idUtilisateur+",heureDebut='"+heureDebut+"',heureFin='"+heureFin+"' WHERE idAgenda="+idAgendaInt+";";
+			String requete = "UPDATE agenda SET idAgenda=" + idAgendaInt + ",evenement='" + textEvent + "',dateDebut='"
+					+ dateDebut + "',dateFin='" + dateFin + "',idUtilisateur=" + idUtilisateur + ",heureDebut='"
+					+ heureDebut + "',heureFin='" + heureFin + "' WHERE idAgenda=" + idAgendaInt + ";";
 			resultat = statement.executeUpdate(requete);
-			
-			
-			Popup Succes = new Popup("Mis à jour", 800,200);
-			
-			JPanel panelSucces = new JPanel(); 
+
+			Popup Succes = new Popup("Mis à jour", 800, 200);
+
+			JPanel panelSucces = new JPanel();
 			JLabel labelSucces = new JLabel("L'évenement a été correctement mise à jour !");
 			Font font = new Font("Open Sans", Font.PLAIN, 30);
 			// Définition du style
@@ -307,14 +286,12 @@ public class AgendaC {
 			panelSucces.setBackground(new Color(85, 239, 196));
 			panelSucces.setForeground(new Color(96, 191, 96));
 
-			
 			return true;
-		}
-		catch (Exception e){
-			
-			Popup NotSucces = new Popup("L'évenement n'a pas été correctement mise à jour !", 800,100);
-			
-			JPanel panelNotSucces = new JPanel(); 
+		} catch (Exception e) {
+
+			Popup NotSucces = new Popup("L'évenement n'a pas été correctement mise à jour !", 800, 100);
+
+			JPanel panelNotSucces = new JPanel();
 			JLabel labelNotSucces = new JLabel("L'évenement n'a pas été correctement mise à jour !");
 			Font font = new Font("Open Sans", Font.PLAIN, 30);
 			// Définition du style
@@ -326,30 +303,30 @@ public class AgendaC {
 			panelNotSucces.setForeground(new Color(191, 48, 48));
 			return false;
 
-		}
-		finally {
-			
+		} finally {
+
 			if (statement != null) {
 				try {
 					statement.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
 	}
-	
+
 	public static boolean suppressionEvent(int idAgendaInt) {
 		Statement statement = null;
 		try {
-			Connection conn =(Connection) Connecteur.connecteurUL;
+			Connection conn = (Connection) Connecteur.connecteurUL;
 
-		    /* Création de l'objet gérant les requêtes */
+			/* Création de l'objet gérant les requêtes */
 			statement = conn.createStatement();
-			String requete = "DELETE FROM agenda WHERE idAgenda = "+idAgendaInt+";";
+			String requete = "DELETE FROM agenda WHERE idAgenda = " + idAgendaInt + ";";
 
 			int resultat = statement.executeUpdate(requete);
-			Popup Succes = new Popup("Suppression :", 800,200);
-			
-			JPanel panelSucces = new JPanel(); 
+			Popup Succes = new Popup("Suppression :", 800, 200);
+
+			JPanel panelSucces = new JPanel();
 			JLabel labelSucces = new JLabel("L'évenement a été correctement supprimé !");
 			Font font = new Font("Open Sans", Font.PLAIN, 30);
 			// Définition du style
@@ -360,12 +337,11 @@ public class AgendaC {
 			panelSucces.setBackground(new Color(85, 239, 196));
 			panelSucces.setForeground(new Color(96, 191, 96));
 			return true;
-		}
-		catch (Exception e){
-			
-			Popup NotSucces = new Popup("Suppression : erreur ", 800,100);
-			
-			JPanel panelNotSucces = new JPanel(); 
+		} catch (Exception e) {
+
+			Popup NotSucces = new Popup("Suppression : erreur ", 800, 100);
+
+			JPanel panelNotSucces = new JPanel();
 			JLabel labelNotSucces = new JLabel("L'évenement n'a pas été correctement supprimé !");
 			Font font = new Font("Open Sans", Font.PLAIN, 30);
 			// Définition du style
@@ -375,80 +351,73 @@ public class AgendaC {
 
 			panelNotSucces.setBackground(new Color(235, 77, 75));
 			panelNotSucces.setForeground(new Color(191, 48, 48));
-			
-			
+
 			return false;
 
-		}
-		finally {
-			
+		} finally {
+
 			if (statement != null) {
 				try {
 					statement.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
 	}
-	
-	
-	
-	public static  List selectVisiteur( ) {
+
+	public static List selectVisiteur() {
 		Statement statement = null;
 		ResultSet resultat = null;
 		try {
 			List List_SV = (List) new ArrayList<List>();
-			Connection conn =(Connection) Connecteur.connecteurUL;
+			Connection conn = (Connection) Connecteur.connecteurUL;
 
-		    /* Création de l'objet gérant les requêtes */
+			/* Création de l'objet gérant les requêtes */
 			statement = conn.createStatement();
-			
+
 			/* Requête récupérat les comptes rendus du user connecté */
-		    String requete = "SELECT CONCAT( nom,\" \", prenom) as nomVisiteur, idUtilisateur, role from utilisateur where role = 1";
+			String requete = "SELECT CONCAT( nom,\" \", prenom) as nomVisiteur, idUtilisateur, role from utilisateur where role = 1";
 			resultat = statement.executeQuery(requete);
-		    /* Exécution d'une requête de lecture */
-			
-		
-		    /* Récupération des données du résultat de la requête de lecture */
-		    while(resultat.next()) {
-		    	ArrayList<String> visiteurs = (ArrayList<String>) new ArrayList<String>();
+			/* Exécution d'une requête de lecture */
 
-	            String nomVisiteur = resultat.getString("nomVisiteur");
-	            int role = resultat.getInt("role");
-	            
-	            visiteurs.add(nomVisiteur);
-	            visiteurs.add(Integer.toString(role));
-	            
-	            ((ArrayList<List>) List_SV).add((List) visiteurs);
-	            
+			/* Récupération des données du résultat de la requête de lecture */
+			while (resultat.next()) {
+				ArrayList<String> visiteurs = (ArrayList<String>) new ArrayList<String>();
+
+				String nomVisiteur = resultat.getString("nomVisiteur");
+				int role = resultat.getInt("role");
+
+				visiteurs.add(nomVisiteur);
+				visiteurs.add(Integer.toString(role));
+
+				((ArrayList<List>) List_SV).add((List) visiteurs);
 
 			}
-		    if(((ArrayList<List>) List_SV).isEmpty()) {
+			if (((ArrayList<List>) List_SV).isEmpty()) {
 			}
-		    
 
 			return List_SV;
 
-		   
 		}
-		
-		    
-		catch (Exception e){
+
+		catch (Exception e) {
 			return null;
 		}
-		
+
 		finally {
 			if (resultat != null) {
 				try {
 					resultat.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 			if (statement != null) {
 				try {
 					statement.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
-	
+
 	}
 }
-
--- src/controller/CnxBDD.java
+++ src/controller/CnxBDD.java
@@ -13,52 +13,49 @@ import model.User;
 public class CnxBDD {
 	public static Connection connecteurUserLab() {
 		/* Connection � la base de donn�e BDUserLab */
-	    final int INITIAL_POOL_SIZE = 10;
+		final int INITIAL_POOL_SIZE = 10;
 		Statement statement = null;
 		ResultSet resultat = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-		    
-		    Config c = new Config();
 
-		    String url = "jdbc:mysql://"+c.getProp("DB_HOST")+"/"+c.getProp("DB_DATABASE")+"?useSSL=false";
-		    String user = c.getProp("DB_USER");
-		    String passwd = c.getProp("DB_PASSWORD");
-		    
-<<<<<<< HEAD
-		    String url = "jdbc:mysql://localhost/appli_visiteur?useSSL=false";
-		    String user = "root";
-		    String passwd = "root";
+			Config c = new Config();
 
-		    Connecteur.connecteurUL = DriverManager.getConnection(url, user, passwd);
-        	System.out.println("connecteur"+Connecteur.connecteurUL);
+			/*
+			 * String url =
+			 * "jdbc:mysql://"+c.getProp("DB_HOST")+"/"+c.getProp("DB_DATABASE")+
+			 * "?useSSL=false"; String user = c.getProp("DB_USER"); String passwd =
+			 * c.getProp("DB_PASSWORD");
+			 */
 
-		    return Connecteur.connecteurUL; 
-=======
-		    Connection conn = DriverManager.getConnection(url, user, passwd);
-		    System.out.println("Connexion effective � la base BDUserLab!");
-		    return conn; 
->>>>>>> 4732dd12ebd6c6699ea79cf428c419e0761f0fff
-		}
-		catch (Exception e) {
+			String url = "jdbc:mysql://localhost/appli_visiteur?useSSL=false";
+			String user = "root";
+			String passwd = "root";
+
+			Connecteur.connecteurUL = DriverManager.getConnection(url, user, passwd);
+			System.out.println("connecteur" + Connecteur.connecteurUL);
+
+			return Connecteur.connecteurUL;
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("La connexion a eu un probl�me");
 			return null;
-		} 
-		finally {
+		} finally {
 			if (resultat != null) {
 				try {
 					resultat.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 			if (statement != null) {
 				try {
 					statement.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
 	}
-	
+
 	/* Connection � la base de donn�e BDMedocLab */
 	public static Connection connecteurMedocLab() {
 		Statement statement = null;
@@ -66,36 +63,37 @@ public class CnxBDD {
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 
-		    /*String url = "jdbc:mysql://192.168.1.118/bdmedocLab?useSSL=false";
-		    String user = "rootuser";
+			/*
+			 * String url = "jdbc:mysql://192.168.1.118/bdmedocLab?useSSL=false"; String
+			 * user = "rootuser";
+			 * 
+			 * String passwd = "Aristee.2018..//";
+			 */
 
-		    String passwd = "Aristee.2018..//";*/
+			String url = "jdbc:mysql://localhost/appli_visiteur?useSSL=false";
 
-		   String url ="jdbc:mysql://localhost/appli_visiteur?useSSL=false";
+			String user = "root";
+			String passwd = "root";
 
-		    String user = "root";
-		    String passwd = "root";
-
-
-		   Connection conn = DriverManager.getConnection(url, user, passwd);
-		    System.out.println("Connexion effective � la base BDUserLab!");
-		    return conn; 
-		}
-		catch (Exception e) {
+			Connection conn = DriverManager.getConnection(url, user, passwd);
+			System.out.println("Connexion effective � la base BDUserLab!");
+			return conn;
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("La connexion a eu un probl�me");
 			return null;
-		}
-		finally {
+		} finally {
 			if (resultat != null) {
 				try {
 					resultat.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 			if (statement != null) {
 				try {
 					statement.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
 	}
@@ -107,54 +105,55 @@ public class CnxBDD {
 		ResultSet resultat = null;
 		try {
 			conn = connecteurUserLab();
-		    
-		    /* Cr�ation de l'objet g�rant les requ�tes */
-		    statement = conn.createStatement();
-		    
-		    /* Ex�cution d'une requ�te de lecture */
-		    
-		    //N'oubliez pas de mettre des ' ' sur vos variables comme ici pr�sent, j'ai mit 5min avant de comprendre xD
-			resultat = statement.executeQuery("SELECT idUtilisateur, login, password, nom, prenom,role  FROM utilisateur WHERE login='" + login + "' AND password='" + mdp + "';");
+
+			/* Cr�ation de l'objet g�rant les requ�tes */
+			statement = conn.createStatement();
+
+			/* Ex�cution d'une requ�te de lecture */
+
+			// N'oubliez pas de mettre des ' ' sur vos variables comme ici pr�sent, j'ai mit
+			// 5min avant de comprendre xD
+			resultat = statement.executeQuery(
+					"SELECT idUtilisateur, login, password, nom, prenom,role  FROM utilisateur WHERE login='" + login
+							+ "' AND password='" + mdp + "';");
 			statement.setFetchSize(100);
-		
-		    /* R�cup�ration des donn�es du r�sultat de la requ�te de lecture */
-		    if(resultat.next()) {
-	            User.id_utilisateur = resultat.getInt( "idUtilisateur" );
-	            User.nom = resultat.getString( "nom" );
+
+			/* R�cup�ration des donn�es du r�sultat de la requ�te de lecture */
+			if (resultat.next()) {
+				User.id_utilisateur = resultat.getInt("idUtilisateur");
+				User.nom = resultat.getString("nom");
 				User.prenom = resultat.getString("prenom");
 				User.role = resultat.getInt("role");
 				User.connected = true;
-				
+
 				return true;
-	            // Pour faire ca, faut que les attributs de user soit en static, me demander par pourquoi
+				// Pour faire ca, faut que les attributs de user soit en static, me demander par
+				// pourquoi
+			} else {
+				System.out.println("login ou mot de passe incorrect");
 			}
-			else{
-	        	System.out.println("login ou mot de passe incorrect");
-	        }
-		    return true;
-		} 
-		catch (Exception e) {
-		    e.printStackTrace();
-		    System.out.println("La connexion a eu un probl�me");
-		    return false;
-		}
-		finally {
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("La connexion a eu un probl�me");
+			return false;
+		} finally {
 			if (resultat != null) {
 				try {
 					resultat.close();
 					conn.close();
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 			if (statement != null) {
 				try {
 					statement.close();
 					conn.close();
 
-				} catch (SQLException e) { /* ignored */}
+				} catch (SQLException e) {
+					/* ignored */}
 			}
 		}
 	}
-
-
 
 }
